3d95f5beb07853db59ceae9b44b26a3e84d1c288
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2024-04-12 16:02:26 +0000

    Sync LeetCode submission Runtime - 1 ms (71.22%), Memory - 45.8 MB (67.76%)

---
+++ 0042-trapping-rain-water/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        if(n==0)
+        return 0;
+        int[] l=new int[n];
+        int[] r=new int[n];
+        int trap=0;
+
+        l[0]=height[0];
+        for(int i=1;i<n;i++)
+        {
+            l[i]=Math.max(l[i-1],height[i]);
+        }
+
+        r[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            r[i]=Math.max(r[i+1],height[i]);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            int minh=Math.min(l[i],r[i]);
+            trap+=minh-height[i];
+        }
+        return trap;
+    }
+}
