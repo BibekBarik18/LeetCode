d9800b2c30283fc719ae578d99db2920be2825b1
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2024-08-13 17:16:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.2 MB (80.01%)

---
+++ 0080-remove-duplicates-from-sorted-array-ii/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int c=1,j=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==nums[i-1])
+            c++;
+            else
+            c=1;
+            if(c<=2)
+            {
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
