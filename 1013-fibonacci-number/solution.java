7436715bd8571f9dad5cbd05b090322088d39d89
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2024-09-09 10:51:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.5 MB (19.34%)

---
+++ 1013-fibonacci-number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n<2)
+        return n;
+        int[] db=new int[n+1];
+        db[0]=0;
+        db[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            db[i]=db[i-1]+db[i-2];
+        }
+        return db[n];
+    }
+}
