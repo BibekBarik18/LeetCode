f4fe0b955e5e78d4cb7a1b9939cbfde0feeb6388
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2024-09-11 13:39:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (9.99%)

--- 0027-remove-element/solution.java
+++ 0027-remove-element/solution.java
@@ -1,20 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int r=nums.length-1,c=0;
+        int k=0;
+        
         for(int i=0;i<nums.length;i++)
         {
-            for(int j=0;j<=r;j++)
+            if(nums[i]!=val)
             {
-                if(nums[j]==val)
-                {
-                    int t=nums[j];
-                    nums[j]=nums[r];
-                    nums[r]=t;
-                    r--;
-                    c++;
-                }
+                nums[k]=nums[i];
+                k=k+1;
             }
         }
-        return nums.length-c;
+        return k;
     }
 }
