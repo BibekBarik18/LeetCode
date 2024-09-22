8dd6c0d937d5cfb5b80637e7e5a561c130b28ab4
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2024-09-22 13:48:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.7 MB (33.30%)

--- 0058-length-of-last-word/solution.java
+++ 0058-length-of-last-word/solution.java
@@ -1,14 +1,6 @@
 class Solution {
-    public int lengthOfLastWord(String s) {
-        s = s.replaceAll("\\s+$", "");
-        char ci=' ';
-        int c=0;
-        int i=s.length()-1;
-        while(i>=0 && s.charAt(i)!=ci)
-        {
-            c++;
-            i--;
-        }
-        return c; 
+    public int lengthOfLastWord(String s1) {
+        String[] s = s1.split(" ");
+        return s[s.length-1].length();
     }
 }
