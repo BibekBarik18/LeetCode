068ccd0367ad2538a539e970cf57f69df0c20d70
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-01-11 13:42:11 +0000

    Sync LeetCode submission Runtime - 1 ms (99.48%), Memory - 45.1 MB (70.31%)

---
+++ 0412-fizz-buzz/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> arr=new ArrayList<>();
+        int i=1;
+        while(i<=n)
+        {
+            if(i%3==0 && i%5==0)
+            arr.add("FizzBuzz");
+            else if(i%3==0)
+            arr.add("Fizz");
+            else if(i%5==0)
+            arr.add("Buzz");
+            else 
+            arr.add(Integer.toString(i));
+            i++;
+        }
+        return arr;
+    }
+}
