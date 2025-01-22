37682303ae07888d3743539642fc5eb1734b83ae
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-01-22 04:08:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.1 MB (42.68%)

--- 0258-add-digits/solution.java
+++ 0258-add-digits/solution.java
@@ -1,20 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-        while(num>=10)
-        {
-            int s=0;
-            int a=num;
-            while(a>0)
-            {
-                int d=a%10;
-                s=s+d;
-                a=a/10;
-            }
-            if(s<10)
-            return s;
-            else
-            num=s;
-        }
-        return num;
+        if(num%9==0 && num>0)
+        return 9;
+        return (num%9);
     }
 }
