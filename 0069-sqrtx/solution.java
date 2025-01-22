a9d2c48d9ded4266a6e950958e221ba024beba11
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-01-22 17:20:02 +0000

    Sync LeetCode submission Runtime - 53 ms (5.03%), Memory - 44.3 MB (6.85%)

---
+++ 0069-sqrtx/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int s=0;
+        int e=x;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            System.out.println(mid);
+            long sq=(long)mid*mid;
+            if(sq==x)
+            return mid;
+            else if(sq<x)
+            s=mid+1;
+            else 
+            e=mid-1;
+        }
+        return e;
+    }
+}
