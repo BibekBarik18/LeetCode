58157afe3aa4af60ca7038c4063dda2328e08f61
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-01-24 03:42:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.6 MB (52.76%)

--- 0461-hamming-distance/solution.java
+++ 0461-hamming-distance/solution.java
@@ -1,22 +1,12 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int max=Math.max(x,y);
-        int min=Math.min(x,y);
-        String maxb=Integer.toBinaryString(max);
-        String minb=Integer.toBinaryString(min);
-        int c=0,b=0;
-        int i=minb.length()-1;
-        int j=maxb.length()-1;
-        while(i>=0)
+        int ans=0;
+        int xor=x^y;
+        while(xor!=0)
         {
-            if(maxb.charAt(j)!=minb.charAt(i))
-            c++;
-            if(maxb.charAt(j)=='1')
-            b++;
-            i--;
-            j--;
+            ans+=xor&1;
+            xor>>=1;
         }
-        c=c+Integer.bitCount(max)-b;
-        return c;
+        return ans;
     }
 }
