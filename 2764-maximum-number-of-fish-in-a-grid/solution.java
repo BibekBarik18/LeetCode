62b30cde30f196b8f778c23c44cdc7e2d4396f40
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-01-29 18:43:19 +0000

    Sync LeetCode submission Runtime - 3 ms (100.00%), Memory - 44.9 MB (67.25%)

---
+++ 2764-maximum-number-of-fish-in-a-grid/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findMaxFish(int[][] grid) {
+        int max = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] > 0 ){
+                    max = Math.max(max, dfs(grid, i, j));
+                }
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] <= 0) {
+            return 0;
+        }
+        
+        int fishCount = grid[i][j];
+        grid[i][j]=-1;
+
+        fishCount += dfs(grid,  i, j + 1);  
+        fishCount += dfs(grid,  i + 1, j);  
+        fishCount += dfs(grid,  i, j - 1);  
+        fishCount += dfs(grid,  i - 1, j); 
+        
+        return fishCount;
+    }
+}
+
