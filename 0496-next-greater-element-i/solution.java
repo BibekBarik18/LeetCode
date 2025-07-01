df256d83c5d60507ab2e854ec6d8b89db4de4839
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-07-01 05:36:42 +0000

    Sync LeetCode submission Runtime - 3 ms (92.44%), Memory - 44.7 MB (11.97%)

--- 0496-next-greater-element-i/solution.java
+++ 0496-next-greater-element-i/solution.java
@@ -1,25 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans=new int[nums1.length];
-        Arrays.fill(ans,-1);
-        for(int i=0;i<nums1.length;i++){
-            int max=nums1[i];
-            int r=nums2.length;
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j]==nums1[i])
-                r=j;
-                if(j>=r)
-                {
-                    if(nums2[j]>max)
-                    {
-                        max=nums2[j];
-                        break;
-                    }
-                }
-            }
-            if(max!=nums1[i])
-            ans[i]=max;
+        Map<Integer,Integer> map=new HashMap<>();
+        Stack<Integer> stack=new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=nums2[i])
+            stack.pop();
+            if(stack.isEmpty())
+            map.put(nums2[i],-1);
+            else
+            map.put(nums2[i],stack.peek());
+            stack.push(nums2[i]);
         }
-        return ans;
+        for(int i=0;i<nums1.length;i++){
+            nums1[i]=map.get(nums1[i]);
+        }
+        return nums1;
     }
 }
