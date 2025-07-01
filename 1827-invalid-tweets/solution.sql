83c3cf73c4d80c90437ec3278f9e5178d12db646
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-07-01 10:28:21 +0000

    Sync LeetCode submission Runtime - 829 ms (7.41%), Memory - 0.0B (100.00%)

--- 1827-invalid-tweets/solution.sql
+++ 1827-invalid-tweets/solution.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-Select tweet_id from Tweets where LENGTH(content)>15;
+-- Select tweet_id from Tweets where (Select count(content) from Tweets)>15
+Select Tweet_id from Tweets where length(content)>15
