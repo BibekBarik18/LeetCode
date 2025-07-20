c133d6e5d8baa538753bab51bdf0c8b33670dcea
BibekBarik18 <145594890+BibekBarik18@users.noreply.github.com>
2025-07-20 06:20:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.3 MB (59.08%)

--- 0048-rotate-image/solution.java
+++ 0048-rotate-image/solution.java
@@ -1,20 +1,18 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) { 
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp=matrix[j][i];
+                matrix[j][i]=matrix[i][j];
+                matrix[i][j]=temp;
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++)
-            {
-                int tmp=matrix[i][j];
-                matrix[i][j]=matrix[i][n-1-j];
-                matrix[i][n-1-j]=tmp;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix[0].length-j-1];
+                matrix[i][matrix[0].length-j-1]=temp;
             }
         }
     }
